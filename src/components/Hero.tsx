bc605bc90c930cb9ee194250b7e7b5f3f5dd46bd
Dasiy <91099769+Dasiy1234@users.noreply.github.com>
2025-08-04 00:09:52 -0400

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -33,7 +33,7 @@ export const Hero = () => {
           </h1>
           
           <p className="text-xl md:text-2xl text-muted-foreground max-w-3xl mx-auto leading-relaxed font-light">
-            A website for everything MarDev makes, all in one cool website - there apps tools to even custom mailing protocols!
+            A website for everything MarDev makes, all in one cool website - there apps, tools to even custom mailing protocols!
             Just a cool website for MarDev Projects. btw no ads 😭🙏
           </p>
         </div>
